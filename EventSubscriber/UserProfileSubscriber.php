36ca8884f624fc377528399fe124f0516ae7e38e
Tannaz <97254921+tannazma@users.noreply.github.com>
2024-11-04 17:20:56 +0100

    Read only user preferences for non admins (#3)
    * Only admins can have user preferences
    
    * Only admins can change the user preference and users can only read it not change it
    
    * Changing the height of the widget
    
    * Fix editability of html when the form is readonly
    
    ---------
    
    Co-authored-by: Tannaz Mohamadi Akbari <tannaz@delftsolutions.nl>

--- EventSubscriber/UserProfileSubscriber.php
+++ EventSubscriber/UserProfileSubscriber.php
@@ -6,6 +6,7 @@ use App\Entity\UserPreference;
 use App\Event\UserPreferenceEvent;
 use App\Event\PrepareUserEvent;
 use App\Repository\TimesheetRepository;
+use Symfony\Component\Security\Core\Security;
 use DateTime;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
@@ -15,10 +16,12 @@ use Symfony\Component\Form\Extension\Core\Type\TextType;
 class UserProfileSubscriber implements EventSubscriberInterface
 {
     protected $repository;
+    private $security;
 
-    public function __construct(TimesheetRepository $repository)
+    public function __construct(TimesheetRepository $repository, Security $security)
     {
         $this->repository = $repository;
+        $this->security = $security;
     }
 
     public static function getSubscribedEvents(): array
@@ -35,30 +38,36 @@ class UserProfileSubscriber implements EventSubscriberInterface
             return;
         }
 
+        // Check if the user has the ROLE_ADMIN role
+        $isAdmin = $this->security->isGranted('ROLE_ADMIN');
+
         $event->addPreference(
             (new UserPreference('target-weekly-hours', 32))
-                // ->setName('target-weekly-hours')
                 ->setValue(32)
                 ->setType(IntegerType::class)
+                ->setOptions(['label' => 'Target Weekly Hours' . ($isAdmin ? '' : ' (Read-Only)'), 'attr' => ['readonly' => !$isAdmin]]) // Make readonly if not admin
         );
+
         $event->addPreference(
             (new UserPreference('target-weekly-start', '1970-01-30'))
-                // ->setName('target-weekly-start')
                 ->setValue('1970-01-30')
                 ->setType(TextType::class)
+                ->setOptions(['label'    => 'Target Weekly Start Date' . ($isAdmin ? '' : ' (Read-Only)'), 'attr' => ['readonly' => !$isAdmin]]) // Make readonly if not admin
         );
+
         $event->addPreference(
             (new UserPreference('yearly-fte-vacation-days', 35))
-                // ->setName('yearly-fte-vacation-days')
                 ->setValue(35)
                 ->setType(NumberType::class)
+                ->setOptions(['label' => 'Yearly Vacation Days' . ($isAdmin ? '' : ' (Read-Only)'), 'attr' => ['readonly' => !$isAdmin]]) // Make readonly if not admin
         );
+
         $event->addPreference(
             (new UserPreference('start-of-period-vacation-hours', 0))
-                // ->setName('start-of-period-vacation-hours')
                 ->setValue(0)
                 ->setType(NumberType::class)
-        );
+                ->setOptions(['label' => 'Start of Period Vacation Hours' . ($isAdmin ? '' : ' (Read-Only)'), 'attr' => ['readonly' => !$isAdmin]]) // Make readonly if not admin
+            );
     }
 
     public function loadUserProfile(PrepareUserEvent $event)
@@ -68,18 +77,19 @@ class UserProfileSubscriber implements EventSubscriberInterface
         }
 
         $accounting_start = strtotime($user->getPreferenceValue('target-weekly-start'));
-        if ($accounting_start === false)
+        if ($accounting_start === false) {
             return;
+        }
         $startDate = new DateTime();
         $startDate->setTimestamp($accounting_start);
 
         $seconds_elapsed = time() - $accounting_start;
-        if ($seconds_elapsed < 0)
+        if ($seconds_elapsed < 0) {
             return;
+        }
         $endDate = new DateTime();
 
         $fte_ratio = $user->getPreferenceValue('target-weekly-hours', 0) / 40.0;
-
         $leftover_hours = $user->getPreferenceValue('start-of-period-vacation-hours', 0);
 
         // Not accounting for leap years
@@ -87,14 +97,12 @@ class UserProfileSubscriber implements EventSubscriberInterface
         $vacation_hours_per_second = 24 * $user->getPreferenceValue('yearly-fte-vacation-days') / $year_length;
 
         $earned_hours = $fte_ratio * $seconds_elapsed * $vacation_hours_per_second;
-
         $total_vacation_hours = $leftover_hours + $earned_hours;
 
         $week_length = 7 * 24 * 60 * 60;
         $elapsed_weeks = $seconds_elapsed / $week_length;
 
         $expected_work_hours = $elapsed_weeks * $fte_ratio * 40;
-
         $worked_hours = $this->repository->getStatistic('duration', $startDate, $endDate, $user) / 60 / 60;
 
         $work_left = $expected_work_hours - $total_vacation_hours - $worked_hours;
