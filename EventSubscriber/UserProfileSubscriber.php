5c02166ac4a356bc77d57a3f001e5a75b58c4bd6
tannazma <97254921+tannazma@users.noreply.github.com>
2024-11-08 11:39:47 +0100

    Non-admin users can see the userprefences but can not edit the data

--- EventSubscriber/UserProfileSubscriber.php
+++ EventSubscriber/UserProfileSubscriber.php
@@ -43,41 +43,88 @@ class UserProfileSubscriber implements EventSubscriberInterface
 
         $event->addPreference(
             (new UserPreference('target-weekly-hours', 32))
-                ->setValue(32)
+                ->setEnabled($isAdmin)
                 ->setType(IntegerType::class)
                 ->setOptions(['label' => 'Target Weekly Hours' . ($isAdmin ? '' : ' (Read-Only)'), 'attr' => ['readonly' => !$isAdmin]]) // Make readonly if not admin
         );
 
         $event->addPreference(
             (new UserPreference('target-weekly-start', '1970-01-30'))
-                ->setValue('1970-01-30')
+                ->setEnabled($isAdmin)
                 ->setType(TextType::class)
                 ->setOptions(['label'    => 'Target Weekly Start Date' . ($isAdmin ? '' : ' (Read-Only)'), 'attr' => ['readonly' => !$isAdmin]]) // Make readonly if not admin
         );
 
         $event->addPreference(
             (new UserPreference('yearly-vacation-hours', 168))
-                ->setValue(35)
+                ->setEnabled($isAdmin)
                 ->setType(NumberType::class)
                 ->setOptions(['label' => 'Yearly Vacation Hours' . ($isAdmin ? '' : ' (Read-Only)'), 'attr' => ['readonly' => !$isAdmin]]) // Make readonly if not admin
         );
 
         $event->addPreference(
             (new UserPreference('start-of-period-vacation-hours', 0))
-                ->setValue(0)
+                ->setEnabled($isAdmin)
                 ->setType(NumberType::class)
                 ->setOptions(['label' => 'Start of Period Vacation Hours' . ($isAdmin ? '' : ' (Read-Only)'), 'attr' => ['readonly' => !$isAdmin]]) // Make readonly if not admin
-            );
+        );
 
         $event->addPreference(
             (new UserPreference('extra-vacation-hours', 0))
+                ->setEnabled($isAdmin)
                 ->setType(IntegerType::class)
-                ->setOptions(['attr' => ['disabled' => !$isAdmin]]) // Make readonly if not admin
+                ->setOptions(['label' => 'Extra Vacation Hours' . ($isAdmin ? '' : ' (Read-Only)'), 'attr' => ['readonly' => !$isAdmin]]) // Make readonly if not admin
         );
     }
 
     public function loadUserProfile(PrepareUserEvent $event)
     {
+        $isAdmin = $this->security->isGranted('ROLE_ADMIN');
+
+        if(!$isAdmin) {
+          // if user is not admin, we show them an unsubmittable fake input, so they know their vacation information
+          echo "<script>
+          async function showVacationInformation() {
+            // for some reason this script is included twice in the page, so we break if its already loaded
+            if (window.DS_VacationData) return;
+
+            window.DS_VacationData = {
+                'Target Weekly Hours': " . $event->getUser()->getPreferenceValue('target-weekly-hours') . ",
+                'Target Weekly Start': " . $event->getUser()->getPreferenceValue('target-weekly-start') . ",
+                'Yearly Vacation Hours': " . $event->getUser()->getPreferenceValue('yearly-vacation-hours') . ",
+                'Start of Period Vacation Hours': " . $event->getUser()->getPreferenceValue('start-of-period-vacation-hours') . ",
+                'Extra Vacation Hours': " . $event->getUser()->getPreferenceValue('extra-vacation-hours') . ",
+            };
+
+            let form;
+            while(!form) {
+              await new Promise(resolve => setTimeout(resolve, 10));
+              form = document.querySelector('form');
+            }
+
+            const formBody = form.querySelector('.card-body');
+
+            Object.entries(window.DS_VacationData).forEach(([preferenceLabel, preferenceValue]) => {
+              // we mimic how the form controls on Kimai preference page looks like
+              const formControl = document.createElement('div');
+              formControl.className = 'mb-3 row';
+              formControl.innerHTML = `
+                  <label class=\"col-form-label col-sm-2\">
+                    \${preferenceLabel} (Read-Only)
+                  </label>
+                  <div class=\"col-sm-10\">
+                    <input readonly=\"readonly\" disabled=\"disabled\" class=\"form-control\" value=\"\${preferenceValue}\">
+                  </div>
+              `;
+
+              formBody.appendChild(formControl);
+            })
+          }
+
+          showVacationInformation();
+          </script>";
+        }
+
         if (null === ($user = $event->getUser())) {
             return;
         }
