a2b2ecffd4414d6ac45f38ee1b62480fecaebdf4
Wouter Groen <wa.groen@gmail.com>
2024-12-13 15:43:06 +0100

    Move script injection to ThemeEventSubscriber - part 1

--- EventSubscriber/UserProfileSubscriber.php
+++ EventSubscriber/UserProfileSubscriber.php
@@ -1,4 +1,4 @@
-<?php
+﻿<?php
 
 namespace KimaiPlugin\VacationHoursBundle\EventSubscriber;
 
@@ -79,53 +79,6 @@ class UserProfileSubscriber implements EventSubscriberInterface
 
     public function loadUserProfile(PrepareUserEvent $event)
     {
-        $isAdmin = $this->security->isGranted('ROLE_ADMIN');
-
-        if(!$isAdmin) {
-          // if user is not admin, we show them an unsubmittable fake input, so they know their vacation information
-          echo "<script>
-          async function showVacationInformation() {
-            if (window.DS_VacationData) {
-              // The `loadUserProfile` is usually called multiple times,
-              // and we need to bail out if this function is executed once.
-              return;
-            }
-
-            // we want to show the inputs to user only on prefs page
-            if (!window.location.href.endsWith('/prefs')) return;
-
-            window.DS_VacationData = {
-                'Target Weekly Hours': " . $event->getUser()->getPreferenceValue('target-weekly-hours') . ",
-                'Target Weekly Start': " . $event->getUser()->getPreferenceValue('target-weekly-start') . ",
-                'Yearly Vacation Hours': " . $event->getUser()->getPreferenceValue('yearly-vacation-hours') . ",
-                'Start of Period Vacation Hours': " . $event->getUser()->getPreferenceValue('start-of-period-vacation-hours') . ",
-                'Extra Vacation Hours': " . $event->getUser()->getPreferenceValue('extra-vacation-hours') . ",
-            };
-
-            const form = document.querySelector('form');
-            const formBody = form.querySelector('.card-body');
-
-            Object.entries(window.DS_VacationData).forEach(([preferenceLabel, preferenceValue]) => {
-              // we mimic how the form controls on Kimai preference page looks like
-              const formControl = document.createElement('div');
-              formControl.className = 'mb-3 row';
-              formControl.innerHTML = `
-                  <label class=\"col-form-label col-sm-2\">
-                    \${preferenceLabel} (Read-Only)
-                  </label>
-                  <div class=\"col-sm-10\">
-                    <input readonly=\"readonly\" disabled=\"disabled\" class=\"form-control\" value=\"\${preferenceValue}\">
-                  </div>
-              `;
-
-              formBody.appendChild(formControl);
-            })
-          }
-
-          document.addEventListener('DOMContentLoaded', showVacationInformation);
-          </script>";
-        }
-
         if (null === ($user = $event->getUser())) {
             return;
         }
@@ -161,6 +114,6 @@ class UserProfileSubscriber implements EventSubscriberInterface
         $expected_work_hours = $elapsed_weeks * $fte_ratio * 40;
         $worked_hours = $this->repository->getStatistic('duration', $startDate, $endDate, $user) / 60 / 60;
 
-        $work_left = $expected_work_hours - $total_vacation_hours - $worked_hours;   
+        $work_left = $expected_work_hours - $total_vacation_hours - $worked_hours;
     }
 }
